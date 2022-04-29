71889cb1552e9b7d0fffaf0a69f2e4c0e9c73968
kpovel <kar.pavel123@gmail.com>
2022-04-29 12:50:11 +0300

    refactored code

--- js/main.js
+++ js/main.js
@@ -1,91 +1,72 @@
-import {UI_ELEMENT} from "./view.js";
+import {UI_ELEMENT, changeStatusTask} from "./view.js";
 
-UI_ELEMENT.addHighTask.addEventListener('click', addTaskOnButton)
-UI_ELEMENT.addLowTask.addEventListener('click', addTaskOnButton)
-UI_ELEMENT.inputHigh.addEventListener('keydown', addTaskOnEnter)
-UI_ELEMENT.inputLow.addEventListener('keydown', addTaskOnEnter)
+UI_ELEMENT.addHighTask.addEventListener('click', addTaskOnButton);
+UI_ELEMENT.addLowTask.addEventListener('click', addTaskOnButton);
+UI_ELEMENT.inputHigh.addEventListener('keydown', addTaskOnEnter);
+UI_ELEMENT.inputLow.addEventListener('keydown', addTaskOnEnter);
 
-let priority
 
 function addTaskOnButton() {
-    priority = this.parentElement.parentElement.id
-    addTask()
+    const priority = this.parentElement.parentElement.id;
+    addTask(priority);
 }
 
 function addTaskOnEnter(e) {
-    if (e.code !== 'Enter') return
-    priority = this.parentElement.parentElement.id
+    if (e.code !== 'Enter') return;
 
-    addTask()
+    const priority = this.parentElement.parentElement.id;
+    addTask(priority);
 }
 
 
-function addTask() {
-    const clone = UI_ELEMENT.templateElement.content.cloneNode(true)
+function addTask(priority) {
+    const clone = UI_ELEMENT.templateElement.content.cloneNode(true);
     if (priority === 'high') {
-        let task = UI_ELEMENT.inputHigh.value
+        const task = UI_ELEMENT.inputHigh.value;
 
         try {
             if (!task.trim()) {
-                throw new Error('no task name')
+                throw new Error('no task name');
             }
-        } catch (e) {
-            console.error(e.message)
-            return
+        }
+        catch (e) {
+            console.error(e.message);
+            return;
         }
 
-        clone.querySelector('.task-name').textContent = task
+        clone.querySelector('.task-name').textContent = task;
 
-        UI_ELEMENT.addedHighTask.prepend(clone)
-        UI_ELEMENT.inputHigh.value = null
+        UI_ELEMENT.addedHighTask.prepend(clone);
+        UI_ELEMENT.inputHigh.value = null;
     } else if (priority === 'low') {
-        let task = UI_ELEMENT.inputLow.value
+        const task = UI_ELEMENT.inputLow.value;
 
         try {
             if (!task.trim()) {
-                throw new Error('no task name')
+                throw new Error('no task name');
             }
-        } catch (e) {
-            console.error(e.message)
+        }
+        catch (e) {
+            console.error(e.message);
             return
         }
 
-        clone.querySelector('.task-name').textContent = task
+        clone.querySelector('.task-name').textContent = task;
 
-        UI_ELEMENT.addedLowTask.prepend(clone)
-        UI_ELEMENT.inputLow.value = null
+        UI_ELEMENT.addedLowTask.prepend(clone);
+        UI_ELEMENT.inputLow.value = null;
     }
-    addCloseElem()
-    changeStatusTask()
+    addCloseElem();
+    changeStatusTask();
 }
 
 function addCloseElem() {
-    let tasksToDelete = document.querySelectorAll('.close-icon')
-    for (let task of tasksToDelete) {
+    const tasksToDelete = document.querySelectorAll('.close-icon');
+    for (const task of tasksToDelete) {
         task.addEventListener('click', function () {
-            this.parentNode.remove()
+            this.parentNode.remove();
         })
     }
 }
 
-addCloseElem()
-
-
-function changeStatusTask() {
-    let task = document.querySelectorAll('.checkbox')
-    for (let taskElement of task) {
-        taskElement.addEventListener('click', changeLookTask)
-    }
-}
-
-changeStatusTask()
-
-function changeLookTask() {
-    if (this.checked) {
-        this.parentElement.style.background = '#F4F4F4'
-        this.setAttribute('status', 'checked')
-    } else if (!this.checked) {
-        this.parentElement.style.background = '#fff'
-        this.setAttribute('status', 'noChecked')
-    }
-}
+addCloseElem();
--- js/view.js
+++ js/view.js
@@ -7,3 +7,22 @@ export const UI_ELEMENT = {
     inputLow: document.getElementById('inputLow'),
     templateElement: document.getElementById('template'),
 }
+
+export function changeStatusTask() {
+    let task = document.querySelectorAll('.checkbox');
+    for (let taskElement of task) {
+        taskElement.addEventListener('click', changeLookTask);
+    }
+}
+
+changeStatusTask()
+
+function changeLookTask() {
+    if (this.checked) {
+        this.parentElement.style.background = '#F4F4F4';
+        this.setAttribute('status', 'checked');
+    } else if (!this.checked) {
+        this.parentElement.style.background = '#fff';
+        this.setAttribute('status', 'noChecked');
+    }
+}
